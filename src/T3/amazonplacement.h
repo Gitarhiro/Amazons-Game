480aceecd6ff32308ff56b2ba6d7c12415a7d2ac
Szymon Bedeniczuk <01158593@pw.edu.pl>
2020-11-26 12:55:36 +0100

    this is the actual header file

---
+++ src/T3/amazonplacement.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void placement(int j , int *board);
+/* this function takes the address of the board and the number of the player 
+who is now in the queue, then in it takes the cooridnates, and then changes the value of the
+cell in the array under the inputed address*/
+
