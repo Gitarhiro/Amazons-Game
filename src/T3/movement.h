0838c89f9a2fba81e21105e471c1e12deaeeea8a
Maria Rita Filocha <01158885@pw.edu.pl>
2020-11-26 12:58:12 +0100

    added comments

--- src/T3/movement.h
+++ src/T3/movement.h
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+    function moveAmazon is responsible for moving an Amazon
 
+    player_id   stores variable for player's id
+    x           stores value of length of the board
+    y           stores value of heigth of the board
+    numOfTiles  stores value of how many tiles the player wants to move his amazon
+    direction   stores name of direction in which the amazon will be moved
+    score       an array which stores the values of each player's score
+    board       a 3-dimentional array storing the board
+
+*/
 void moveAmazon(int player_id, int x, int y, int numOfTiles, char direction[], int *score, int *board)
 
