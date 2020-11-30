a9d71061c9bbf423e06620148aa36c56d67f3542
Devansh Das <01165826@pw.edu.pl>
2020-12-01 00:08:36 +0100

    Update src/T4/printBoard.c

--- src/T4/printBoard.c
+++ src/T4/printBoard.c
@@ -1,1 +1,14 @@
-//placeholder
+#include <stdio.h>
+#include <stdlib.h>
+
+void printBoard(int board[][][]) {
+    for (int d=0;d<x;d++) {
+        printf("\n");
+        for (int e=0;e<y;e++) {
+            printf(" ");
+            for (int f=0;f<3;f++) {
+                printf("%d", board[d][e][f]);
+            }
+        }
+    }
+}
