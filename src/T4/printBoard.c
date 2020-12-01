d60b983e88a310344076c83f745b159f1a1be1d5
Devansh Das <01165826@pw.edu.pl>
2020-12-01 18:09:27 +0100

    Update printBoard.c

--- src/T4/printBoard.c
+++ src/T4/printBoard.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printBoard(int board[][][]) {
-    for (int d=0;d<x;d++) {
+int printBoard(int board[2][2][3]) {
+    for (int d=0;d<2;d++) {
         printf("\n");
-        for (int e=0;e<y;e++) {
+        for (int e=0;e<2;e++) {
             printf(" ");
-            for (int f=0;f<3;f++) {
-                printf("%d", board[d][e][f]);
+            if (board[d][e][3] == 0) {
+                printf("☐");
+            }
+            else if (board[d][e][3] == 9) {
+                printf("/");
+            }
+            else {
+                printf("%d", board[d][e][3]);
             }
         }
     }
+
+    return 0;
+}
+
+int main() {
+    int a[2][2][3] = {{{3, 4, 2}, {0, -3, 9}}, {{13, 4, 0}, {3, 1, 9}}};
+    printBoard(a[2][2][3]);
+
+    return 0;
 }
