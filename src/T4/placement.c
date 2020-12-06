24f06fc7a617a07cb6e800d11136b029207db9d9
Szymon Bedeniczuk <01158593@pw.edu.pl>
2020-12-06 19:36:34 +0100

    Changed placement.c to match the structure method our team now uses

--- src/T4/placement.c
+++ src/T4/placement.c
@@ -1,27 +1,20 @@
+
 #include <stdio.h>
 
-void placement(int j , int *board)
-{
+void placement(Tile **board, int j){
     int x,y;
-    while(1)
-    {
-        printf ("x: ");
-        scanf ("%d", &x);
-        printf("\n");
-        printf ("y: ");
-        scanf ("%d", &y);
-        printf("\n");
-
-      if(*(((board+x)+y)+2)==0)
-        {
-            *(((board+x)+y)+2)=j;
-            return;
+    while (true) {
+        printf("\nx: ");
+        scanf("%d" , &x );
+        printf("\ny: ");
+        scanf("%d" , &y );
+        if(board[x][y].occupation == 0) {
+            board[x][y].occupation = j;
+            break;
         }
-        else
-        {
-            printf("invalid placement, try again.\n");
-        }
+        else   printf("\nInvalid placement. Try again.\n")
     }
-
-
+    
+    
 }
+
