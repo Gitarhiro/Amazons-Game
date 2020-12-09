0e6e1712992f57716eb9b7c96c6e590cacb27a45
Maria Filocha <01158885@pw.edu.pl>
2020-12-09 19:47:34 +0100

    Use a structure for board instead of an array

--- src/T4/movement.c
+++ src/T4/movement.c
@@ -1,89 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* 
-    -what if move is not possible?
-    -what about using an artifact? is it a separate function?
-*/
-                                                                              
-void moveAmazon(int player_id, int x, int y, int numOfTiles, char direction[], int *score, int *board)
+                                                                          
+void moveAmazon(int player_id, int x, int y, int numOfTiles, char direction[], Tile **board)
 {
+    //removing an amazon from his previous tile
+    board[x][y].occupation = 0;
+
+    //moving an amazon to a different tile
     if(strcmp(direction, "UP")==0)
     {
-        //removing amazon from his previous spot
-        //tile on board[x][y][2] is now unoccupied
-        *(((board + x) + y) + 2) = 0;
+        if(board[x][y - numOfTiles].occupation == 0)                                //checking if the tile an amazon want to move to is unoccupied
+        {
+            board[x][y - numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
 
-        //tile on board[x][y-h][2] is now occupied
-        *(((board + x) + y - numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-        *(score + player_id) = *(((board + x) + y - numOfTiles) + 0);//how to do it?
+        //using artefact
         
-        /*using the artifact*/
     }
     else if(strcmp(direction, "DOWN")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x) + y + numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x][y + numOfTiles].occupation == 0)                                
+        {
+            board[x][y + numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
     }
     else if(strcmp(direction, "LEFT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x - numOfTiles) + y) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x - numOfTiles][y].occupation == 0)                                
+        {
+            board[x - numOfTiles][y].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
     }
     else if(strcmp(direction, "RIGHT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x + numOfTiles) + y) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x + numOfTiles][y].occupation == 0)                                
+        {
+            board[x + numOfTiles][y].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
+        
     }
     else if(strcmp(direction, "UPLEFT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x - numOfTiles) + y - numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x - numOfTiles][y - numOfTiles].occupation == 0)                                
+        {
+            board[x - numOfTiles][y - numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
+        
     }
     else if(strcmp(direction, "UPRIGHT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x + numOfTiles) + y - numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x + numOfTiles][y - numOfTiles].occupation == 0)                                
+        {
+            board[x + numOfTiles][y - numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
     }
     else if(strcmp(direction, "DOWNLEFT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x - numOfTiles) + y + numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/ 
+        if(board[x - numOfTiles][y + numOfTiles].occupation == 0)                                
+        {
+            board[x - numOfTiles][y + numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
     }
     else if(strcmp(direction, "DOWNRIGHT")==0)
     {
-        *(((board + x) + y) + 2) = 0;
-        *(((board + x + numOfTiles) + y + numOfTiles) + 2) = player_id;
-
-        /*adding score from the tile*/
-
-        /*using the artifact*/
+        if(board[x + numOfTiles][y + numOfTiles].occupation == 0)                                
+        {
+            board[x + numOfTiles][y + numOfTiles].occupation = player_id;
+        }
+        else
+        {
+            printf("Move is invalid.");
+        }
     }
 
 }
