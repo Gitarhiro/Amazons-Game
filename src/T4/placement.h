728e3e9d520e141336fdbb6aae8e5540a36879ec
Szymon Bedeniczuk <01158593@pw.edu.pl>
2020-12-10 11:25:04 +0100

    Update placement.h

--- src/T4/placement.h
+++ src/T4/placement.h
@@ -4,4 +4,4 @@ void placement(Tile **board, int j);
 /* this function takes the address of the board and the number of the player 
 who is now in the queue, then in it takes the cooridnates, and then changes the value of the
 cell in the array under the inputed address*/
-
+/*THING TO BE MADE FOR THE NEXT TUTORIALS IS TO CHECK THE TYPE OF DATA IMPUT IN THE PLACEMENT*/
