7175007b265e9c44ed55476e2741a0301fe79c9a
Szymon Bedeniczuk <01158593@pw.edu.pl>
2020-12-24 20:02:55 +0100

    Update placement.c, included checker code to verify whether the input was valid if it comes to being a positive number IN THE RANGE OF THE BOARD

--- src/T4/placement.c
+++ src/T4/placement.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
-
-void placement(Tile **board, int j){
-    int x,y;
+// DIMENSIONS IS THE RANGE OF THE BOARD//
+void placement(Tile **board, int j, int dimensions){
+    int x,y,r;
     while (1) {
+        r=0;
+        while (r==0) {
         printf("\nx: ");
-        scanf("%d" , &x );
+        r=scanf("%d" ,&x);
+        if(r==0 || x <=0 || x > dimensions ) 
+        {
+            printf("Wrong input, give another positive number in the range: \n");
+            r=0;
+        }
+        while(getchar()!='\n');
+    }
+    r=0;
+    while (r==0) {
         printf("\ny: ");
-        scanf("%d" , &y );
+        r=scanf("%d" ,&y);
+        if(r==0 || y <=0 || y > dimensions ) 
+        {
+            printf("Wrong input, give another positive number in the range: \n");
+            r=0;
+        }
+        while(getchar()!='\n');
+    }
+        
         if(board[x][y].occupation == 0) {
             board[x][y].occupation = j;
             break;
         }
-        else   printf("\nInvalid placement. Try again.\n")
+        else   printf("\nInvalid placement. Try again.\n");
     }
     
+    
 }
-
