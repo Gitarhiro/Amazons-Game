39e66d6aa9fa1d68c719d7f9d1b93877a8ad9c8a
Szymon Bedeniczuk <01158593@pw.edu.pl>
2020-12-24 20:04:10 +0100

    Update placement.h

--- src/T4/placement.h
+++ src/T4/placement.h
@@ -5,3 +5,4 @@ void placement(Tile **board, int j, int dimensions);
 who is now in the queue, then in it takes the cooridnates, and then changes the value of the
 cell in the array under the inputed address*/
 /*THING TO BE MADE FOR THE NEXT TUTORIALS IS TO CHECK THE TYPE OF DATA IMPUT IN THE PLACEMENT*/
+// TYPE OF DATA AND IN WHAT NUMBER IS THE X AND Y IS CHECKED GJ ME //
