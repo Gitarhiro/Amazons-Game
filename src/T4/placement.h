10e79da12cf224b957136a42ed89d5eba88922cc
Devansh Das <01165826@pw.edu.pl>
2020-12-31 21:12:43 +0100

    Deleted src/T4/placement.c, src/T4/placement.h files

--- src/T4/placement.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void placement(Tile **board, int j, int dimensions);
-/* this function takes the address of the board and the number of the player 
-who is now in the queue, then in it takes the cooridnates, and then changes the value of the
-cell in the array under the inputed address*/
-/*THING TO BE MADE FOR THE NEXT TUTORIALS IS TO CHECK THE TYPE OF DATA IMPUT IN THE PLACEMENT*/
-// TYPE OF DATA AND IN WHAT NUMBER IS THE X AND Y IS CHECKED GJ ME //
