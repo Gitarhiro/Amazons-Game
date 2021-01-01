98d439e180f52a6d4bdfd338bb51bf0a98c9ee6a
Devansh Das <01165826@pw.edu.pl>
2021-01-01 11:22:43 +0100

    Update main.c

--- src/T5/main.c
+++ src/T5/main.c
@@ -1,108 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "board.h"
-
-/*void moveAmazon(int player_id, int x, int y, int numOfTiles, char direction[], Tile **board) {
-    //removing an amazon from his previous tile
-    board[x][y].occupation = 0;
-
-    //moving an amazon to a different tile
-    if(strcmp(direction, "UP")==0)
-    {
-        if(board[x][y - numOfTiles].occupation == 0)//checking if the tile an amazon want to move to is unoccupied
-        {
-            board[x][y - numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-
-        //using artefact
-        
-    }
-    else if(strcmp(direction, "DOWN")==0)
-    {
-        if(board[x][y + numOfTiles].occupation == 0)                                
-        {
-            board[x][y + numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-    }
-    else if(strcmp(direction, "LEFT")==0)
-    {
-        if(board[x - numOfTiles][y].occupation == 0)                                
-        {
-            board[x - numOfTiles][y].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-    }
-    else if(strcmp(direction, "RIGHT")==0)
-    {
-        if(board[x + numOfTiles][y].occupation == 0)                                
-        {
-            board[x + numOfTiles][y].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-        
-    }
-    else if(strcmp(direction, "UPLEFT")==0)
-    {
-        if(board[x - numOfTiles][y - numOfTiles].occupation == 0)                                
-        {
-            board[x - numOfTiles][y - numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-        
-    }
-    else if(strcmp(direction, "UPRIGHT")==0)
-    {
-        if(board[x + numOfTiles][y - numOfTiles].occupation == 0)                                
-        {
-            board[x + numOfTiles][y - numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-    }
-    else if(strcmp(direction, "DOWNLEFT")==0)
-    {
-        if(board[x - numOfTiles][y + numOfTiles].occupation == 0)                                
-        {
-            board[x - numOfTiles][y + numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-    }
-    else if(strcmp(direction, "DOWNRIGHT")==0)
-    {
-        if(board[x + numOfTiles][y + numOfTiles].occupation == 0)                                
-        {
-            board[x + numOfTiles][y + numOfTiles].occupation = player_id;
-        }
-        else
-        {
-            printf("Move is invalid.");
-        }
-    }
-}*/
-
+#include "movement.h"
 
 int main() {
     //Declaration of all variables
@@ -173,6 +74,8 @@ int main() {
             if (i == numOfPlayers) {
                 i = 0;
             }
+
+            printf("\n");
         }
 
 return 0;
