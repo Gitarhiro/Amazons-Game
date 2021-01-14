e7230bfa06412c77b76b165ed9c5e939581714c8
Nozimbek Abdugofurov <n.abdugofurov@stud.elka.pw.edu.pl>
2021-01-14 16:33:40 +0100

    Replace movement.h

--- src/Interactive-mode/movement.h
+++ src/Interactive-mode/movement.h
@@ -1,17 +1,15 @@
-#include "assets.h"
+#include <stdio.h>
 
-void moveAmazon(int player_id, int x, int y, int numOfTiles, char direction[], Tile **board, int size, int numOfPlayers, char name[numOfPlayers][11], Score scoreBoard[numOfPlayers]);
+typedef struct {
+    int score;
+    int artefact;
+    int occupation;
+    int moved;
+    } Tile;
 
-void usingArtifact(int player_id, int x, int y, Tile **board, int size, int numOfPlayers, char name[numOfPlayers][11], Score scoreBoard[numOfPlayers]);
+typedef struct {
+        int player;
+        int points;
+    } Score;
 
-void shoot(int id, int x, int y, int numOfTiles, char direction[], Tile **board, int size, int numOfPlayers, char name[numOfPlayers][11], Score scoreBoard[numOfPlayers]);
-
-int isAmazonAvaiable(int x, int y, Tile **board, int size);
-
-int isMovementPossible(int x, int y, Tile **board);
-
-int availableAmazons(int player_id, Tile **board, int size);
-
-int endGameCheck(int playerCanMove[], int players);
-
-void newMove(Tile **board, int size);
+void printBoard(Tile **board, int x, int numOfPlayers, char name[][11], Score scoreBoard[]);
