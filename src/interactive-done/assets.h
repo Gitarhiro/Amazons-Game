ed9f9f868123c260650cf020dbfc4f497f3ffe32
Szymon Bedeniczuk <01158593@pw.edu.pl>
2021-01-20 12:00:55 +0100

    Upload New File

---
+++ src/interactive-done/assets.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    int score;
+    int artefact;
+    int occupation;
+    int moved;
+    } Tile;
+
+typedef struct {
+        int player;
+        int points;
+    } Score;
+
+void printBoard(Tile **board, int x, int numOfPlayers, char name[][11], Score scoreBoard[]);
+
+void printBoardNoScore(Tile **board, int x, int numOfPlayers);
+
+void instruction();
