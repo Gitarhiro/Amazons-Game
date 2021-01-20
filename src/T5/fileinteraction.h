f5cdd94b06d2bebbec980d6835efd39320f0f36e
Nozimbek Abdugofurov <n.abdugofurov@stud.elka.pw.edu.pl>
2021-01-20 18:58:43 +0100

    Upload New File

---
+++ src/T5/fileinteraction.h
@@ -0,0 +1,7 @@
+#ifndef TAUCS_H
+#define TAUCS_H
+#include "assets.h"
+#endif
+
+void inputBoard(FILE* f, Tile** board, int height, int width);
+void outputBoard(FILE* f, Tile** board, int height, int width);
