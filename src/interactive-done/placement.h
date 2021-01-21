e3b396136f86903942e80be0c6d8ce042570fa9d
Maria Rita Filocha <01158885@pw.edu.pl>
2021-01-21 11:43:38 +0100

    added comments

--- src/interactive-done/placement.h
+++ src/interactive-done/placement.h
@@ -4,5 +4,10 @@
 #endif
 
 void randomizeBoard (Tile **board, int x);
+//tile board    -   structure for game's board
+//int x         -   dimension of the square board
 
 void placement(Tile **board, int j, int dimensions);
+//tile board    -   structure for game's board
+//int j         -   id of player which is placing an amazon
+//dimensions    -   dimensions of the square board, dimensions x dimensions
