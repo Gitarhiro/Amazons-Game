7d3ccd69736a128962fd6f6b1c3c8bccaacdf705
Maria Rita Filocha <01158885@pw.edu.pl>
2021-01-21 12:08:48 +0100

    added comments

--- src/interactive-done/assets.h
+++ src/interactive-done/assets.h
@@ -13,7 +13,18 @@ typedef struct {
     } Score;
 
 void printBoard(Tile **board, int x, int numOfPlayers, char name[][11], Score scoreBoard[]);
+//printBoard                function that prints the board and the scoreboard below it
+//tile board            -   strucure of the board
+//x                     -   dimensions of the square board
+//numOfPlayers          -   number of players
+//name[][11]            -   char array of players names
+//scoreBoard[]          -   Score array that stores number of points for each player
 
 void printBoardNoScore(Tile **board, int x, int numOfPlayers);
+//printBoardNoScore         function that prints the board without the scoreboard
+//tile board            -   strucure of the board
+//x                     -   dimensions of the square board
+//numOfPlayers          -   number of players
 
 void instruction();
+//instruction               function that prints the instructions for the game, if asked 
